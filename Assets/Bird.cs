cd79690bf81ca939192060763edf7089e34f92cd
Kieu Truc Tran <kieutruc.tran@student.htw-berlin.de>
2024-10-27 17:14:02 +0100

    add wall collider + UI for score text
    ... when right wall is hit, move left and vice versa

--- Assets/Bird.cs
+++ Assets/Bird.cs
@@ -6,6 +6,8 @@ public class Bird : MonoBehaviour
 {
     public Rigidbody2D rb;
     public float jumpForce = 8f;
+    
+    private bool hitRightwall = false;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +23,26 @@ public class Bird : MonoBehaviour
             rb.velocity = new Vector2(0, jumpForce);
         }
 
-        transform.Translate(Vector2.right * (Time.deltaTime * 2));
+        if(hitRightwall)
+        {
+            transform.Translate(Vector2.left * (Time.deltaTime * 2));
+        }
+        else
+        {
+            transform.Translate(Vector2.right * (Time.deltaTime * 2));
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "rightwall")
+        {
+            hitRightwall = true;
+            transform.localScale = new Vector3(-0.7f, 0.7f, 0.7f);
+        }
+        else {
+            hitRightwall = false;
+            transform.localScale = new Vector3(0.7f, 0.7f, 0.7f);
+        }
     }
 }
